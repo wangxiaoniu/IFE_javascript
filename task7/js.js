4a2027d0c06bf5e1741ab5fe9e40f529364a9183
wangxiaoniu <13519870949@163.com>
2017-04-20 17:17:15 +0800

    777

---
+++ task7/js.js
@@ -0,0 +1,9 @@
+
+
+
+	var box =document.querySelector('.box');
+	var btn1=document.querySelectorAll('button')[0];
+	var btn2=document.querySelectorAll('button')[1];
+	var btn3=document.querySelectorAll('button')[2];
+
+	btn3.style.background='red';
